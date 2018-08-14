ebfa47797651b6f64d8569c110afa016809bde0b
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-08-14 08:29:10 -0500

    Add files via upload

---
+++ 3.1.rkt
@@ -0,0 +1,7 @@
+# lang sicp
+
+;;;;   3.1
+(define (make-accumulator current-value)
+  (lambda (add-amount)
+    (begin (set! current-value (+ add-amount current-value))
+           current-value)))
