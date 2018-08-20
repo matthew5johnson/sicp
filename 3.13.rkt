59bbf7ea700c97d1506030b4ed4727a04ed42755
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-08-20 07:33:30 -0500

    Add files via upload

---
+++ 3.13.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+;;;;   3.13
+(define (cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define z (cycle (list 'a 'b 'c)))
+(cdr z)
+;#0=(mcons 'b (mcons 'c (mcons 'a #0#)))
