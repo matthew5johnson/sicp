12ef0dced154b50e80e06dc51d5a727726b99833
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-08-26 21:59:52 -0500

    Add files via upload

---
+++ 3.19.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+;;;;   3.19
+
+(define (cycle? x) 
+   (define (iter start next l u) 
+     (cond 
+      ((null? next) false) 
+      ((eq? start next) true) 
+      ((< l u) (iter start (cdr next) (+ l 1) u)) 
+      (else (iter next (cdr next) 1 (* 2 u))))) 
+   (if (null? x) 
+       false 
+       (iter x (cdr x) 1 1)))
