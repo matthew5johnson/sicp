9bc1a00bb056913e46fac035022b1abd69f074ac
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-10-31 08:39:44 -0500

    Add files via upload

---
+++ 3.24.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+; 3.3.3 Representing Tables
+
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (cdr record)
+        false)))
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((same-key? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (same-key? key counterpart tolerance)
+  (if (=< (abs(- key counterpart)) tolerance)
+      True
+      False))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table
+                  (cons (cons key value) (cdr table)))))
+  'ok)
+
+(define (make-table same-key?)
+  (define local-table (list '*table*)))
