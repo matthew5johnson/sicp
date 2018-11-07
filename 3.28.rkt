9cff9690bf1768a47f098263cebe134b90e27e05
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-11-06 18:40:46 -0600

    Update 3.28.rkt

--- 3.28.rkt
+++ 3.28.rkt
@@ -1,5 +1,16 @@
 A simulator for Digital Circuits
 
 3.3.4
-
+; 3.28
 #lang sicp
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+          (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                (lambda ()
+                  (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
