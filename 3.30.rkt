ef446cebd64f45e2ab7e64aabdd6550fce670963
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-11-07 19:27:40 -0600

    Create 3.30.rkt

---
+++ 3.30.rkt
@@ -0,0 +1,12 @@
+#lang sicp 
+
+; 3.30
+
+(define (ripple-carry-adder Al Bl Sl C)
+  (define (A B S C1 C2)
+    (if (null? A) 
+        S
+        (begin (full-adder (car A) (car B) C1 (car S) C2)
+               (iter (cdr A) (cdr B) (cdr S) C2 (make-wire)))))
+  (iter A1 B1 S1 C (make-wire)))
+  
