40b14e3d78f154e5758265dac30f65f4e0bf2187
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-11-10 07:55:17 -0600

    Create 3.33.rkt

---
+++ 3.33.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+
+; (/ (+ a b) 2) = c
+
+(define (averager a b c)
+  (let ((u (make-connector))
+        (v (make-connector)))
+     (adder a b u)
+     (multiplier v c u)
+     (constant 2 v)
+     'ok))
