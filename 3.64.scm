6fb7fe9ef73988414f9cfcf31b16198b1ec1546c
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-12-13 07:31:56 -0600

    Update 3.64.scm

--- 3.64.scm
+++ 3.64.scm
@@ -1,4 +1,12 @@
-; write a prcoedure stream-limit that takes as arguments a stream and a number (the tolerance). It should examine the stream until it finds two successive elements that differ in absolute value by less than the tolerance, and return the second of the two elements. Using this, we could compute square roots up to a given tolerance by:
+; write a prcoedure stream-limit that takes as arguments a stream and a number (the tolerance). 
+;It should examine the stream until it finds two successive elements that differ in absolute value by less than the tolerance, 
+;and return the second of the two elements. Using this, we could compute square roots up to a given tolerance by:
 
 (define (sqrt x tolerance)
   (stream-limit (sqrt-stream x) tolerance))
+
+(define (stream-limit s1 tolerance)
+  (if (< (abs (- stream-car stream-cdr)) tolerance)
+      stream-cdr
+      (stream-limit (stream-cdr s1) tolerance)))
+  
