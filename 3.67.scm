879b0d2cb4afba57eaa2d4ab8d64b7faa7108026
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-12-15 08:51:09 -0600

    Create 3.67.scm

---
+++ 3.67.scm
@@ -0,0 +1,16 @@
+; Modify the pairs procedure so that (pairs integers integers) will produce the stream of all pairs of integers. Without the condition i <= j
+; My note: because pairs is only producing the integers above the diagonal in that matrix of all possible pairs
+
+(define (pairs s t)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (interleave
+      (interleave
+        (stream-map (lambda (x) (list (stream-car s) x))
+                  (stream-cdr t))
+        (stream-map (lambda (x) (list (stream-car t) x))
+                  (stream-cdr s))
+        (pairs (stream-cdr s) (stream-cdr t))
+)))))
+      
+      
