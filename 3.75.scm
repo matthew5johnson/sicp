5bc96196f05b281b041c010b8f222d7e5c1f64b0
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-12-22 07:57:26 -0600

    Create 3.75.scm

---
+++ 3.75.scm
@@ -0,0 +1,6 @@
+(define (make-zero-crossings input-stream last-value storage)
+  (let ((avpt (/ (+ (stream-car input-stream) last-value) 2)))
+    (cons-stream (sign-change-detector avpt last-value)
+                 (make-zero-crossings (stream-cdr input-stream)
+                                      (stream-car input-stream)
+                                      avpt))))
