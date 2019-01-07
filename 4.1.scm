0cfdc5c3d1d70da68b39f3585139e6be03040fce
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2019-01-07 11:22:42 -0600

    Update 4.1.scm

--- 4.1.scm
+++ 4.1.scm
@@ -1,6 +1,11 @@
 ; Write a version of list-of-values that evaluates operands from left to right regardless of the order of evaluation in the underlying Lisp. Also write a version of list-of-values that evaluates operands from right to left
 
-?????????????
+; original list-of-values
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
 ; R -> L
 (define (list-of-values exps env) 
    (if (no-operand? exps) 
@@ -8,3 +13,11 @@
        (let ((L (eval (rest-operands exps) env)) 
                  (R (eval (first-operand exps) env))) 
          (cons L R))))
+
+; L -> R
+(define (list-of-values exps env) 
+   (if (no-operand? exps) 
+       '() 
+       (let ((R (eval (rest-operands exps) env)) 
+                 (L (eval (first-operand exps) env))) 
+         (cons R L))))
