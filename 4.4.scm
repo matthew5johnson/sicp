8e76f80769ee4bffe2d2517eb57ce0197087a1b4
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2019-01-10 08:24:34 -0600

    Update 4.4.scm

--- 4.4.scm
+++ 4.4.scm
@@ -5,8 +5,8 @@
         ((assignment? exp) (eval-assignment exp env))
         ((definition? exp) (eval-definition exp env))
         ((if? exp) (eval-if exp env))
-        ((and? exp) (eval-and exp env))
-        ((or? exp) (eval-or exp env))
+        ((and? exp) (eval-and (cdr exp) env))
+        ((or? exp) (eval-or (cdr exp) env))
         ((lambda? exp)
          (make-procedure (lambda-parameters exp)
                          (lambda-body exp)
@@ -19,3 +19,16 @@
                 (list-of-values (operands exp) env)))
         (else
          (error "Unknown expression type -- EVAL" exp))))
+
+(define (eval-and remaining env)
+  (let next (previous true)
+    (if (and (null? remaining) previous)
+        previous
+        (next (eval (car remaining) env) (cd remaining env))))))
+
+(define (eval-or remaining env)
+  (let next (previous false)
+    (if (or (null? remaining) previous)
+        remaining
+        (next (eval (car remaining) env) (cd remaining env))))))
+  
