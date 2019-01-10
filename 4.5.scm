40f9fbedb2982a7ac4dba006b52e81f2f4906e42
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2019-01-10 08:51:41 -0600

    Create 4.5.scm

---
+++ 4.5.scm
@@ -0,0 +1,60 @@
+; Modify the handling of cond so that it supports this extended syntax:
+
+(cond ((assoc 'b '((a 1) (b 2))) => cadr)
+      (else false))
+
+; returns 2
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((extended-cond? exp) (eval 
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+         (error "Unknown expression type -- EVAL" exp))))
+         
+(define (cond? exp) (tagged-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+  
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+ 
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      'false
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp (cond-actions first))
+                (error "ELSE clause isn't last -- COND->IF" clauses))
+        (if (eq? (cadr (cond-actions first)) '=>))
+            (make-if (cond-predicate first)
+                   (list (caddr first) (cond-predicate first))
+                   (expand-clauses rest))
+            (make-if (cond-predicate first)
+                     (sequence->exp (cond-actions first))
+                     (expand-clauses rest)))))))
+
+; expressions that we choose to implement as syntactic transformations are called derived expressions
+ 
